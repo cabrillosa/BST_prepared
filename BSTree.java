f90465b4f441755f012e4c8d6ef836725b9261c9
Artemio Cabrillos Jr <act.acabrillos@gmail.com>
2020-12-14 13:03:49 +0800

    refactor code and added comments

--- BSTree.java
+++ BSTree.java
@@ -1,13 +1,40 @@
 public class BSTree {
     Node root;
 
+    /* PUBLIC API FUNCTIONS */
     public void add(int data) {
         root = addRecursive(root, data);
     }
 
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
+    }
+
+    public boolean searchBST(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    public void displayInorder() {
+        displayInorderRecursive(root);
+    }
+
+    public void displayPreorder() {
+        displayPreorderRecursive(root);
+    }
+
+    public void displayPostorder() {
+        displayPostorderRecursive(root);
+    }
+
+    /* PRIVATE/RECURSIVE FUNCTIONS */
+    private Node createNode(int data) {
+        return new Node(data);
+    }
+
     private Node addRecursive(Node current, int data) {
+        // if reached the correct spot, insert the newnode
         if (current == null) {
-            return new Node(data);
+            return createNode(data);
         }
 
         if (data < current.data) {
@@ -19,18 +46,19 @@ public class BSTree {
     }
 
     private boolean containsNodeRecursive(Node current, int value) {
+        // value NOT found return false
         if (current == null) {
             return false;
         }
+        // value is found, return true
         if (value == current.data) {
             return true;
         }
-        return value < current.data ? containsNodeRecursive(current.left, value)
-                : containsNodeRecursive(current.right, value);
-    }
-
-    private int findSmallestValue(Node root) {
-        return root.left == null ? root.data : findSmallestValue(root.left);
+        if (value < current.data) {
+            return containsNodeRecursive(current.left, value);
+        } else {
+            return containsNodeRecursive(current.right, value);
+        }
     }
 
     private Node deleteRecursive(Node current, int value) {
@@ -38,14 +66,17 @@ public class BSTree {
             return null;
         }
 
+        // value searched is found. decide which case to implement : case 1 2 or 3?
         if (value == current.data) {
+            // case 1: delete a node without a child
             if (current.left == null && current.right == null) {
                 return null;
-            } else if (current.right == null) {
+            } else if (current.right == null) { // case2: delete a node with 1 child (either left or right child)
                 return current.left;
             } else if (current.left == null) {
                 return current.right;
-            } else {
+            } else { // case 3: delete a node with two child
+                //find the smallest successor
                 int smallestValue = findSmallestValue(current.right);
                 current.data = smallestValue;
                 current.right = deleteRecursive(current.right, smallestValue);
@@ -53,24 +84,14 @@ public class BSTree {
             }
         }
 
+        // call itself until value to delete is found or it reached the end of leaf node
         if (value < current.data) {
             current.left = deleteRecursive(current.left, value);
             return current;
+        } else {
+            current.right = deleteRecursive(current.right, value);
+            return current;
         }
-        current.right = deleteRecursive(current.right, value);
-        return current;
-    }
-
-    public void delete(int value) {
-        root = deleteRecursive(root, value);
-    }
-
-    public boolean searchBST(int value) {
-        return containsNodeRecursive(root, value);
-    }
-
-    public void display() {
-        displayInorderRecursive(root);
     }
 
     private void displayInorderRecursive(Node n) {
@@ -78,6 +99,31 @@ public class BSTree {
             displayInorderRecursive(n.left);
             System.out.println(n.data);
             displayInorderRecursive(n.right);
+        }
+    }
+
+    private void displayPreorderRecursive(Node n) {
+        if (n != null) {
+            System.out.println(n.data);
+            displayPreorderRecursive(n.left);
+            displayPreorderRecursive(n.right);
+        }
+    }
+
+    private void displayPostorderRecursive(Node n) {
+        if (n != null) {
+            displayPostorderRecursive(n.left);
+            displayPostorderRecursive(n.right);
+            System.out.println(n.data);
+        }
+    }
+
+    /* UTILITY FUNCTION */
+    private int findSmallestValue(Node root) {
+        if (root.left == null) {
+            return root.data;
+        } else {
+            return findSmallestValue(root.left);
         }
     }
 }
--- MainDriver.java
+++ MainDriver.java
@@ -1,16 +1,35 @@
 public class MainDriver {
     public static void main(String[] args) {
+        // instantiate binary search tree
         BSTree bst = new BSTree();
-        bst.add(20);
-        bst.add(15);
         bst.add(10);
-        bst.add(25);
-        bst.add(5);
-        bst.add(30);
-        bst.add(2);
-        bst.display();
-        System.out.println("searching.. 10-> "+bst.searchBST(100));
-        bst.delete(1);
-        bst.display();
+        bst.add(6);
+        bst.add(20);
+        bst.add(4);
+        bst.add(8);
+        bst.add(15);
+        bst.add(21);
+        bst.add(15);
+        bst.add(21);
+        bst.add(11);
+        bst.add(15);
+        bst.add(22);
+        bst.add(14);
+        bst.add(17);
+        System.out.println("Displaying In-order:");
+        bst.displayInorder();
+        System.out.println("Displaying Pre-order:");
+        bst.displayPreorder();
+        System.out.println("Displaying Post-order:");
+        bst.displayPostorder();
+        System.out.println("searching.. 11-> " + bst.searchBST(4));
+        System.out.println("Deleting 20");
+        bst.delete(20);
+        System.out.println("Displaying In-order:");
+        bst.displayInorder();
+        System.out.println("Displaying Pre-order:");
+        bst.displayPreorder();
+        System.out.println("Displaying Post-order:");
+        bst.displayPostorder();
     }
 }
